7266d91c1da044a0bbf450d9c7833ec628bac6de
Darshini-SB <sbdarshini5@gmail.com>
2025-06-25 15:44:00 +0530

    Add files via upload

---
+++ SINGLETON/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Singleton a = Singleton.getInstance();
+        Singleton b = Singleton.getInstance();
+        a.show();
+    }
+}
